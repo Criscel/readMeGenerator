58f10d56f1f3270c015f413f76e00ea2057349e8
Criscel Agdeppa <cel_agdeppa@yahoo.com>
2021-02-10 20:44:53 +1100

    initial commit

---
+++ Develop/index.js
@@ -0,0 +1,80 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js')
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        message: "Enter Project Title: ",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "Enter Project Description: ",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "Enter Project Deployed Link: ",
+        name: "link",
+    },
+    {
+        type: "input",
+        message: "Enter Project Installation Instruction: ",
+        name: "instruction",
+    },
+    {
+        type: "input",
+        message: "Enter Project Usage Information: ",
+        name: "information",
+    },
+    {
+        type: "input",
+        message: "Enter Project Contribution Guidelines: ",
+        name: "guidlines",
+    },
+    {
+        type: "input",
+        message: "Enter Project Test Instruction: ",
+        name: "test",
+    },
+    {
+        type: "list",
+        message: "Choose desired license for Application: ",
+        choices: ['MIT', 'BSD license', 'Apache', 'No License'],
+        name: "license",
+    },
+    {
+        type: "input",
+        message: "Enter Github Username: ",
+        name: "username",
+    },
+    {
+        type: "input",
+        message: "Enter E-mail address: ",
+        name: "email",
+    },
+     
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeToFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log('ReadMe successfully created')
+    )
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((data) => {
+        const genMarkdown = generateMarkdown(data);
+        writeToFile('README.md', genMarkdown);
+    })
+}
+
+// Function call to initialize app
+init();
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # readMeGenerator
+This ReadMe generator basically creates a ReadMe file for user just by entering couple of information regarding their projects. 
